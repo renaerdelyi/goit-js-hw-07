4f2993b0c743aec9d23677b02654a7fa0e4d8f79
renaerdelyi <rena.szabo@yahoo.com>
2024-08-29 19:02:50 +0300

    modificare1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,42 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+
+const createGalleryMarkup = (items) => {
+    return items
+    .map((item) => {
+        const{preview, original, description} = item;
+        return`
+ <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    })
+    .join("");
+};
+
+
+const renderGallery = () => {
+   galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+};
+renderGallery();
+
+function onGalleryClick(event){
+    event.preventDefault();
+    const isGalleryImg =event.target.classList.contains("gallery__image")
+
+    if(!isGalleryImg)return;
+    const imgSource = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+        <img src="${imgSource}" width="auto"/>`);
+        instance.show();
+}
+galleryContainer.addEventListener("click",onGalleryClick)
