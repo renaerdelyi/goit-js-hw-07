d1c03c3f1e7969b7aa411ef68cf7197cdb5e2c7f
renaerdelyi <rena.szabo@yahoo.com>
2024-09-05 12:14:02 +0300

    modificare2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const modalGallery = document.querySelector(".gallery")
+
+const createGallery = (photos) => {
+    return photos
+    .map((photos) => {
+        const{preview, original, description} = photos;
+        return `
+        <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      data-source="${original}
+      alt="${description}" />
+   </a>
+</li> `;    
+    })
+    .join("");
+}
+
+const renderGallery = () => {
+    modalGallery.innerHTML = createGallery(galleryItems);
+ };
+ renderGallery();
+ 
+ const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt', 
+    captionDelay: 250,  
+  })
